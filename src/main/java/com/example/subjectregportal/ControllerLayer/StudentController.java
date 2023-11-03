f9d18904faeb5ca84f94a8b664c4e50212cae34c
Sandeep Kumar <sandeepkumar090304@gmail.com>
2023-11-03 14:12:38 +0530

    2nd Commit

--- src/main/java/com/example/subjectregportal/ControllerLayer/StudentController.java
+++ src/main/java/com/example/subjectregportal/ControllerLayer/StudentController.java
@@ -5,12 +5,11 @@ import com.example.subjectregportal.ServiceLayer.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class StudentController  {
 
     @Autowired
@@ -21,7 +20,7 @@ public class StudentController  {
     }
 
     @GetMapping("/api/get")
-    public List<Student> getStudent(){
+    public List<Student> StudentDetails(){
         return studentService.getSt();
     }
 
